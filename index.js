7bcee6a5afbe9ee2abd5ddc8e09fee128aed0430
Tyler Algigi <44593496+tyleralgigi@users.noreply.github.com>
2021-06-03 16:33:03 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import express from 'express';
+import {buildFederatedSchema} from "@apollo/federation";
+import mongoose from 'mongoose';
+import {resolvers} from "./resolvers";
+import {typeDefs} from "./typeDefs";
+
+const startServer = async () =>{
+    const app = express();
+
+    app.get('/', function (req, res) {
+        res.send('Hello World!');
+    });
+
+
+    
+    const server = new ApolloServer({
+        // These will be defined for both new and existing servers
+        typeDefs,
+        resolvers
+    });
+    
+    server.applyMiddleware({app}); //app is from an existing express app
+    
+    //contecting to mongoDB
+    await mongoose.connect('YOUR MONGO URL', {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true
+    });
+
+    app.listen({port: 80}, () => 
+        console.log(`server is ready at http://localhost:80${server.graphqlPath}`)
+    );
+
+
+};
+
+startServer();
+
+/*
+       context: () => {
+            return { 
+                eventLoader: new DataLoader(async keys => {
+                    const events = await Event.findById
+                })
+            }
+        }
+        */

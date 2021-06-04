352b18e35398441f934f6bb58a85f52767d83ec5
Tyler <>
2021-06-04 09:23:40 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const startServer = async () =>{
     server.applyMiddleware({app}); //app is from an existing express app
     
     //contecting to mongoDB
-    await mongoose.connect('mongodb+srv://tyler:Football93%21@cluster0.cpci2.mongodb.net/testing', {
+    await mongoose.connect('TESTING', {
         useNewUrlParser: true, 
         useUnifiedTopology: true
     });
